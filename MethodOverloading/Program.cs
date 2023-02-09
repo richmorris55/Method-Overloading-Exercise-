f93196aeadaf6486f3e6e062409d9016a4e7ee85
Richard Morris <richmorris55@yahoo.com>
2023-02-08 21:19:10 -0800

    InitialCommit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 2, true)); 
         }
+
+        //Step 1
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+
+        }
+
+
+        //Step 2
+        public static decimal Add(decimal num1, decimal num2)
+        {
+
+            return num1 + num2;
+
+
+        }
+
+
+        //Step 3
+        public static string Add(int num1, int num2, bool isMoney)
+        {
+            if (isMoney == true && num1 + num2 > 1) 
+            {
+                return $"{num1 + num2} dollars";
+            }
+
+            else if (isMoney == true && num1 + num2 == 1)
+            {
+                return $"{num1 + num2} dollar";
+            }
+            else
+            {
+                return $"{num1+num2}";
+            }
+           
+           
+
+        }
+
+
+
     }
 }
